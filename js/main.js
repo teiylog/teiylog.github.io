ac397386d0a15baa3cb5fc97906892a8dfd7598e
teiylog <55331682+teiylog@users.noreply.github.com>
2019-09-26 10:22:26 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,18 +6,28 @@ let app = new Vue({
 		neruna_count: 1,
 		nero: '現在の寝ろ: 0',
 		neruna: '現在の寝るな: 0',
-		ratio: '1 : 1'
+		ratio: '50 : 50'
 	},
 	methods: {
 		onClickNero: function() {
 			let nero_str = this.nero_count.toLocaleString();
 			++this.nero_count;
 			this.nero = '現在の寝ろ: ' + nero_str;
+
+			let max_value = this.nero_count + this.neruna_count;
+			let f_ratio = Math.round( this.nero_count / max_value * 100 * 10 ) / 10;
+			let s_ratio = Math.round( this.neruna_count / max_value * 100 * 10 ) / 10;
+			this.ratio = '' + f_ratio + ' : ' + s_ratio;
 		},
 		onClickNeruna: function() {
 			let neruna_str = this.neruna_count.toLocaleString();
 			++this.neruna_count;
 			this.neruna = '現在の寝るな: ' + neruna_str;
+
+			let max_value = this.nero_count + this.neruna_count;
+			let f_ratio = Math.round( this.nero_count / max_value * 100 * 10 ) / 10;
+			let s_ratio = Math.round( this.neruna_count / max_value * 100 * 10 ) / 10;
+			this.ratio = '' + f_ratio + ' : ' + s_ratio;
 		}
 	}
 });
